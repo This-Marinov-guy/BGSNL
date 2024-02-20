f02281bd82207ea73b12c860512facac796424db
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-02-20 07:52:43 +0200

    fix2

--- src/pages/eventActions/NonMemberPurchase.jsx
+++ src/pages/eventActions/NonMemberPurchase.jsx
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect, useState } from "react";
 import * as yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useHttpClient } from "../../hooks/http-hook";
-import { useHttpFetchClient } from "../../hooks/http-hook";
+import { useHttpFetchClient } from "../../hooks/fetch-hook";
 import PageHelmet from "../../component/common/Helmet";
 import Header from "../../component/header/Header";
 import ScrollToTop from "react-scroll-up";
