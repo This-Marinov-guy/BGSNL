45ab54e5a0ad1e05842423d716371fb9083a6e25
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-02-28 07:59:59 +0200

    modified:   src/elements/portfolio/PortfolioList.jsx

--- src/elements/portfolio/PortfolioList.jsx
+++ src/elements/portfolio/PortfolioList.jsx
@@ -5,34 +5,38 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 const PortfolioList = (props) => {
 
-  const {region} = useParams();
+  const { region } = useParams();
 
   return (
-    <React.Fragment>
-      {props.target.map((value, index) => (
-        <div className="portfolio portfolio-slider" key={index}>
-          <Link
-            to={
-              props.style === "society"
-                ? `/${region}/event-details/${value.title}`
-                : `/${region}/other-event-details/${value.title}`
-            }
-            className={
-              props.style === "society"
-                ? "thumbnail-inner"
-                : "thumbnail-inner-2"
-            }
-          >
-            <ImageFb
-              className="thumbnail portfolio-img"
-              src={`${value.thumbnail}.webp`}
-              fallback={`${value.thumbnail}.jpg`}
-              alt="Event Images"
-            />
-          </Link>
+    <div className="row">
+      <div className="col-lg-12">
+        <div className="slick-space-gutter--15 slickdot--100">
+          {props.target.map((value, index) => (
+            <div className="portfolio portfolio-slider" key={index}>
+              <Link
+                to={
+                  props.style === "society"
+                    ? `/${region}/event-details/${value.title}`
+                    : `/${region}/other-event-details/${value.title}`
+                }
+                className={
+                  props.style === "society"
+                    ? "thumbnail-inner"
+                    : "thumbnail-inner-2"
+                }
+              >
+                <ImageFb
+                  className="thumbnail portfolio-img"
+                  src={`${value.thumbnail}.webp`}
+                  fallback={`${value.thumbnail}.jpg`}
+                  alt="Event Images"
+                />
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
-    </React.Fragment>
+      </div>
+    </div>
   );
 };
 
