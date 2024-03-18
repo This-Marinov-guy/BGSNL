27345823049ef34fc934d2454ef44e6d6eeb1e6a
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-03-18 08:23:29 +0200

    modified:   src/elements/ui/RegionOptions.jsx

--- src/elements/ui/RegionOptions.jsx
+++ src/elements/ui/RegionOptions.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from '../../component/functional/RegionLayout.module.css'
 import { Link, useParams } from 'react-router-dom'
 import capitalizeFirstLetter from '../../util/capitalize'
-import { REGIONS_MEMBERSHIP } from '../../util/REGIONS_AUTH_CONFIG'
+import { REGIONS } from '../../util/REGIONS_DESIGN'
 
 const RegionOptions = (props) => {
 
@@ -12,7 +12,7 @@ const RegionOptions = (props) => {
     <div className="mt--200">
                 <h3 style={{ textAlign: 'center', fontFamily: 'Archive' }}>{region ? `Welcome to ${capitalizeFirstLetter(region)}` : 'Choose your Region'}</h3>
                 <ul className="brand-style-2">
-                {REGIONS_MEMBERSHIP.map((region, index) => {
+                {REGIONS.map((region, index) => {
                     return <li key={index} className={styles[region]}>
                         <Link to={`/${region}/${props.to}`}>
                             <button  className={' rn-button-style--2 btn-solid'}>{region}</button>
