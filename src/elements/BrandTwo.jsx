e9ad9d757e60e3c53b6fcab5a9ce802e353be9e3
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-05-13 21:18:42 +0300

    modified:   public/assets/scss/header/_header.scss 	modified:   src/elements/BrandTwo.jsx

--- src/elements/BrandTwo.jsx
+++ src/elements/BrandTwo.jsx
@@ -18,9 +18,9 @@ class BrandTwo extends Component {
                     <li>
                         <a href='https://studybuddy.bg/' target='_blank'><img src="/assets/images/brand/brand-04.png" alt="Logo Images" /></a>
                     </li>
-                    <li>
+                    {/* <li>
                         <a href='https://www.integral.bg/' target='_blank'><img src="/assets/images/brand/brand-05.png" alt="Logo Images" /></a>
-                    </li>
+                    </li> */}
                     <li>
                         <a href='https://en.unify.bg/' target='_blank'><img src="/assets/images/brand/brand-02.png" alt="Logo Images" /></a>
                     </li>
