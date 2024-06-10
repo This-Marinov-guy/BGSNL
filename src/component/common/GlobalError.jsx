fbc5ce4edc94043548d184faf8ed10dfc56d361c
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-06-10 20:36:06 +0300

    modified:   src/component/common/GlobalError.jsx

--- src/component/common/GlobalError.jsx
+++ src/component/common/GlobalError.jsx
@@ -8,26 +8,33 @@ const ErrorFallback = ({ resetErrorBoundary }) => {
     const navigate = useNavigate();
 
     return (
-        <div className="container">
-            <div className="mt--200">
-                <h3 className="center_text mb--10">Something went wrong - If the issue persists please report it!</h3>
-                <h5 className="center_text mb--80">Your feedback helps us make the platform better. We appreciate it!</h5>
-                <div className="options-btns-div mt--60">
-                    <button
-                        onClick={resetErrorBoundary}
-                        className="rn-button-style--2 btn-solid"
-                    >
-                        Try again
-                    </button>
-                    <button
-                        onClick={() => navigate(-1)}
-                        className="rn-button-style--2 rn-btn-reverse"
-                    >
-                        Go Back
-                    </button>
+        <>
+            <HeaderTwo
+                headertransparent="header--transparent"
+                colorblack="color--black"
+                logoname="logo.png"
+            />
+            <div className="container">
+                <div className="mt--200">
+                    <h3 className="center_text mb--10">Something went wrong - If the issue persists please report it!</h3>
+                    <h5 className="center_text mb--80">Your feedback helps us make the platform better. We appreciate it!</h5>
+                    <div className="options-btns-div mt--60">
+                        <button
+                            onClick={resetErrorBoundary}
+                            className="rn-button-style--2 btn-solid"
+                        >
+                            Try again
+                        </button>
+                        <button
+                            onClick={() => navigate(-1)}
+                            className="rn-button-style--2 rn-btn-reverse"
+                        >
+                            Go Back
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
@@ -35,19 +42,12 @@ const GlobalError = ({ children }) => {
     const location = useLocation();
 
     return (
-        <>
-            {/* <HeaderTwo
-                headertransparent="header--transparent"
-                colorblack="color--black"
-                logoname="logo.png"
-            /> */}
-            <ErrorBoundary
-                FallbackComponent={ErrorFallback}
-                key={location.pathname} // Force remount on location change
-            >
-                {children}
-            </ErrorBoundary>
-        </>
+        <ErrorBoundary
+            FallbackComponent={ErrorFallback}
+            key={location.pathname} // Force remount on location change
+        >
+            {children}
+        </ErrorBoundary>
     );
 };
 
