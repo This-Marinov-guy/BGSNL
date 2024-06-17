09406783af059c92ead2d06fffb50e3f4799c501
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-06-17 19:14:28 +0300

    new file:   public/assets/images/portfolio/groningen/portfolio-40.jpg 	new file:   public/assets/images/portfolio/groningen/portfolio-40.webp 	new file:   public/assets/images/tickets/groningen/ticket-40.jpg 	modified:   src/util/OPEN_EVENTS.js

--- src/util/OPEN_EVENTS.js
+++ src/util/OPEN_EVENTS.js
@@ -3,7 +3,45 @@ import { Field, ErrorMessage } from "formik";
 
 export const SOCIETY_EVENTS = {
   groningen: [
+    {
+      is_tickets_closed: false,
+      membersOnly: false,
+      visible: true,
+      title: "Bulgarian Student Party",
+      // newTitle: 'Bulgarian Dinner',
+      description: "Let's wrap the student year",
+      bgImage: "16",
+      date: "27th June",
+      time: "22:00",
+      ticketTimer: '2024-06-22T22:00:00',
+      ticketLimit: 100,
+      // Use the corrected date and time for changes in the date or time. Do not change the initial ones as it will make a new event in the DB
+      // correctedDate: "",
+      // correctedTime: "",
+      where: `Sunny Beach`,
+      entry: 5,
+      memberEntry: 4,
+      including: [],
+      // ticket_link: '',
+      price_id: 'price_1PSi6aIOw5UGbAo1EZ762u3s',
+      memberPrice_id: 'price_1PSi6kIOw5UGbAo1TFquLoce',
+      activeMemberPrice_id: 'price_1PSi6uIOw5UGbAo1OUbvNXqr',
+      freePass: ['vladislavmarinov3142@gmail.com'],
+      marketingInputs: false,
+      extraInputs: false,
+      text: [
+        `Come join us on the 27th of June at @sunnybeachgroningen to celebrate the end of exam season and warm up for the summer.🥳🏖
+`,
+`Time to throw those textbooks, put on your best outfits and celebrate the end of the school year.❌📚
+`,
+`We’ve secured only the best for you. The amazing @djzander and @sashkoang@balkanify.nl will be playing from all time favourites to new summer hits.🎉
+`,
 
+      ],
+      ticket_img: '/assets/images/tickets/groningen/ticket-40.jpg',
+      images: ["/assets/images/portfolio/groningen/portfolio-40"],
+      thumbnail: "/assets/images/portfolio/groningen/portfolio-40",
+    },
   ],
   rotterdam: [
     // {
@@ -45,13 +83,13 @@ export const SOCIETY_EVENTS = {
     //   thumbnail: "/assets/images/portfolio/rotterdam/portfolio-4",
     // },
   ],
-  leeuwarden: [  ],
+  leeuwarden: [],
   breda: [
   ],
   maastricht: [
 
   ],
   amsterdam: [
-    
+
   ]
 }
