c0f721961e5bd87afc327ffb7fa8ba4ed4588ea7
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-09-02 22:10:46 +0300

    update

--- src/elements/ui/modals/Locked.jsx
+++ src/elements/ui/modals/Locked.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHttpClient } from "../../../hooks/http-hook";
 import Loader from "../loading/Loader";
 import { Link } from "react-router-dom";
-import ModalWindow from "./ModalWindow";
+import { Dialog } from 'primereact/dialog';
 import { useDispatch, useSelector } from "react-redux";
 import SubscriptionManage from "../buttons/SubscriptionManage";
 import { REGIONS_MEMBERSHIP_SPECIFICS, findMembershipByProperty } from "../../../util/defines/REGIONS_AUTH_CONFIG";
@@ -99,7 +99,7 @@ const Locked = () => {
       {loading ? <Loader /> : <span>Manage Subscription</span>}
     </button> 
     :
-    <div className="center_div_col" style={{ gap: '15px' }}>
+    <div className="center_div_col mt--20" style={{ gap: '15px' }}>
       {REGIONS_MEMBERSHIP_SPECIFICS.map((option, index) => {
         return <button
           key={index}
@@ -114,24 +114,24 @@ const Locked = () => {
   }
 
   return (
-    <ModalWindow static="static" show={currentUser.status === 'locked'} freeze>
-      <div style={{ padding: "40px" }} className="center_section">
-        <h2>
+    <Dialog modal visible={currentUser.status === 'locked'} blockScroll={true} closable={false}>
+      <div className="center_section">
+        <h3>
           {currentUser.status === "locked"
             ? "Your account is locked!"
             : "Your account is suspended!"}
-        </h2>
+        </h3>
         <p className="center_text">
           {currentUser.status === "locked"
             ? "To continue using the benefits of a member please make the subscription payment (cancel anytime)! Otherwise, log out of your account."
             : "We have noticed some violation from your side. Unfortunately, we will need to block your account until further notice. Please contact: bgsn.tech.nl@gmail.com"}
         </p>
         {currentUser.status === "locked" && actionButtons}
-        <Link to="/" className="rn-button-style--2 rn-btn-green mt--40">
+        <Link to="/" className="rn-button-style--2 rn-btn-reverse mt--40">
           Back to Home
         </Link>
       </div>
-    </ModalWindow>
+    </Dialog>
   );
 };
 
