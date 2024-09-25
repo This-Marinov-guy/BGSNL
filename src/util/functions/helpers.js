792817edff61c35a4eb489c3a3088cafff8d8f42
Vladislav Marinov <104770295+This-Marinov-guy@users.noreply.github.com>
2024-09-25 09:35:46 +0300

    Update helpers.js

--- src/util/functions/helpers.js
+++ src/util/functions/helpers.js
@@ -106,19 +106,19 @@ export const estimatePriceByEvent = (selectedEvent, user = {}, blockDiscounts = 
     const isMember = !!user && user.token;
     const isActiveMember = isMember && checkAuthorization(user.token, ACCESS_4);
     const isMemberDataFull = user?.name && user?.surname && user?.email;
-    const includedText = isMember ? (selectedEvent.hasOwnProperty(memberIncluding) ? `(including ${selectedEvent.memberIncluding})` : '') : (selectedEvent.hasOwnProperty(entryIncluding) ? `(including ${selectedEvent.entryIncluding})` : '');
+    const includedText = isMember ? (selectedEvent.hasOwnProperty('memberIncluding') ? `(including ${selectedEvent.memberIncluding})` : '') : (selectedEvent.hasOwnProperty('entryIncluding') ? `(including ${selectedEvent.entryIncluding})` : '');
 
     if (selectedEvent.isFree) {
         price = 'FREE'
     } else if (selectedEvent.ticketLink) {
         price = 'Check ticket portal'
     } else if (isActiveMember && product?.activeMember.price) {
-        price = product?.activeMember.price + (!isNaN(product?.activeMember.price) ? ' euro (extra discounted)' : ' ') + includedText
+        price = product?.activeMember.price + (!isNaN(product?.activeMember.price) ? ' euro (extra discounted)' : ' ') + includedText;
     } else if (isMember && (product?.member.price || selectedEvent.isMemberFree)) {
         price = selectedEvent.isMemberFree ? 'FREE'
-            : product?.member.price + (!isNaN(product?.member.price) ? ' euro (discounted)' : ' ') + includedText
+            : product?.member.price + (!isNaN(product?.member.price) ? ' euro (discounted)' : ' ') + includedText;
     } else if (product?.guest.price) {
-        price = product?.guest.price + (!isNaN(product?.guest.price) ? ' euro ' : ' ') + includedText
+        price = product?.guest.price + (!isNaN(product?.guest.price) ? ' euro ' : ' ') + includedText;
     } else {
         price = 'TBA'
     }
