e29dbebd550935885d87b5c9926b87c10a4941a9
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-10-02 22:40:18 +0300

    up

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import { store } from "./redux/store";
-import { PrimeReactProvider } from 'primereact/api';
+import { PrimeReactProvider } from "primereact/api";
 import { isProd } from "./util/functions/helpers";
 import PageLoading from "./elements/ui/loading/PageLoading";
 import RegionLayout from "./layouts/common/RegionLayout";
 import { removeLogsOnProd } from "./util/functions/helpers";
 import AuthLayout from "./layouts/authentication/AuthLayout";
 import { removeModal, showModal } from "./redux/modal";
-import { ACCESS_1, ACCESS_2, ACCESS_3, ACCESS_4, INACTIVITY_MODAL, JWT_RESET_TIMER, LOCAL_STORAGE_SESSION_LIFE, LOCAL_STORAGE_USER_DATA, SESSION_TIMEOUT, WARNING_THRESHOLD } from "./util/defines/common";
+import {
+  ACCESS_1,
+  ACCESS_2,
+  ACCESS_3,
+  ACCESS_4,
+  INACTIVITY_MODAL,
+  JWT_RESET_TIMER,
+  LOCAL_STORAGE_SESSION_LIFE,
+  LOCAL_STORAGE_USER_DATA,
+  SESSION_TIMEOUT,
+  WARNING_THRESHOLD,
+} from "./util/defines/common";
 import InactivityModal from "./elements/ui/modals/InactivityModal";
 import { calculateTimeRemaining } from "./util/functions/date";
 import { login, logout, selectUser } from "./redux/user";
@@ -21,17 +32,19 @@ import { isObjectEmpty } from "./util/functions/helpers";
 import { startPageLoading, stopPageLoading } from "./redux/loading";
 
 // Style
-import './index.scss'
+import "./index.scss";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import MainLayout from "./layouts/MainLayout";
 import GlobalError from "./component/common/GlobalError";
 import { useJWTRefresh } from "./hooks/api-hooks";
 
-// Pages  
+// Pages
 const Home = lazy(() => import("./pages/Home"));
 const Toni = lazy(() => import("./pages/information/articles/Toni"));
 const Minerva = lazy(() => import("./pages/information/articles/Minerva"));
-const StudentMigration = lazy(() => import("./pages/information/articles/StudentMigration"));
+const StudentMigration = lazy(() =>
+  import("./pages/information/articles/StudentMigration")
+);
 const About = lazy(() => import("./pages/information/About"));
 const Developers = lazy(() => import("./pages/information/Developers"));
 const Contact = lazy(() => import("./pages/information/Contact"));
@@ -66,19 +79,11 @@ const ContestRegister = lazy(() =>
   import("./pages/eventActions/ContestRegister")
 );
 
-const AddEvent = lazy(() =>
-  import("./pages/userActions/AddEvent")
-);
-const EditEvent = lazy(() =>
-  import("./pages/userActions/EditEvent")
-);
-const EventDashboard = lazy(() =>
-  import("./pages/userActions/EventDashboard")
-);
+const AddEvent = lazy(() => import("./pages/userActions/AddEvent"));
+const EditEvent = lazy(() => import("./pages/userActions/EditEvent"));
+const EventDashboard = lazy(() => import("./pages/userActions/EventDashboard"));
 
-const GuestPurchase = lazy(() =>
-  import("./pages/eventActions/GuestPurchase")
-);
+const GuestPurchase = lazy(() => import("./pages/eventActions/GuestPurchase"));
 const GuestCheck = lazy(() => import("./pages/redirects/CheckTicket"));
 const Success = lazy(() => import("./pages/redirects/Success"));
 const SuccessDonation = lazy(() => import("./pages/redirects/SuccessDonation"));
@@ -94,7 +99,7 @@ const PageNavigationFunc = () => {
   }, [pathname]);
 
   return null;
-}
+};
 
 const Root = () => {
   const [timeRemaining, setTimeRemaining] = useState(SESSION_TIMEOUT);
@@ -111,7 +116,7 @@ const Root = () => {
   const onLogout = () => {
     dispatch(removeModal(INACTIVITY_MODAL));
     dispatch(logout());
-  }
+  };
 
   const loginUser = async (jwtToken) => {
     try {
@@ -125,25 +130,36 @@ const Root = () => {
       }
 
       // TODO: temp fix as we cannot access the token in the hook
-      const responseData = await sendRequest('user/get-subscription-status', 'GET', {}, { Authorization: `Bearer ${jwtToken}` });
+      const responseData = await sendRequest(
+        "user/get-subscription-status",
+        "GET",
+        {},
+        { Authorization: `Bearer ${jwtToken}` }
+      );
 
       dispatch(
         login({
           token: jwtToken,
-          ...responseData
+          ...responseData,
         })
       );
     } catch (err) {
     } finally {
       setIsLoading(false);
     }
-  }
+  };
+
+  const clearUserStorage = () => {
+    localStorage.removeItem(LOCAL_STORAGE_SESSION_LIFE);
+    localStorage.removeItem(LOCAL_STORAGE_USER_DATA);
+  };
 
   let target = Date.now() + SESSION_TIMEOUT;
 
   useEffect(() => {
     let storedUser = JSON.parse(localStorage.getItem(LOCAL_STORAGE_USER_DATA));
-    let expirationTime = localStorage.getItem(LOCAL_STORAGE_SESSION_LIFE) ?? Date.now();
+    let expirationTime =
+      localStorage.getItem(LOCAL_STORAGE_SESSION_LIFE) ?? Date.now();
 
     if (
       !isObjectEmpty(storedUser) &&
@@ -153,6 +169,8 @@ const Root = () => {
       expirationTime > Date.now()
     ) {
       loginUser(storedUser.token);
+    } else {
+      clearUserStorage();
     }
   }, []);
 
@@ -180,7 +198,7 @@ const Root = () => {
 
         // Check remaining time every second
         intervalCheck = setInterval(() => {
-          const timeLeft = calculateTimeRemaining(target) // Calculate time remaining
+          const timeLeft = calculateTimeRemaining(target); // Calculate time remaining
 
           setTimeRemaining(timeLeft);
 
@@ -218,17 +236,17 @@ const Root = () => {
   // end auth session logic
 
   if (process.env.REACT_APP_MAINTENANCE == true) {
-    return <Maintenance />
+    return <Maintenance />;
   }
 
   if (isLoading) {
-    return <PageLoading />
+    return <PageLoading />;
   }
 
   return (
     <BrowserRouter basename={"/"}>
       <PageNavigationFunc />
-      <InactivityModal timeRemaining={timeRemaining}/>
+      <InactivityModal timeRemaining={timeRemaining} />
       <GlobalError>
         <Suspense fallback={<PageLoading />}>
           <Routes>
@@ -238,8 +256,16 @@ const Root = () => {
             <Route exact path={`/developers`} element={<Developers />} />
             <Route exact path={`/rules-and-regulations`} element={<Policy />} />
             <Route exact path={`/articles/toni-villa`} element={<Toni />} />
-            <Route exact path={`/articles/from-bulgaria-to-the-netherlands`} element={<StudentMigration />} />
-            <Route exact path={`/articles/acedemie-minerva`} element={<Minerva />} />
+            <Route
+              exact
+              path={`/articles/from-bulgaria-to-the-netherlands`}
+              element={<StudentMigration />}
+            />
+            <Route
+              exact
+              path={`/articles/acedemie-minerva`}
+              element={<Minerva />}
+            />
             {/* <Route exact path={`/active-member`} >
                 <ActiveMember />
               </Route> */}
@@ -250,68 +276,139 @@ const Root = () => {
 
             {/* <Route exact path={`/:region/board`} element={<RegionLayout><Board /></RegionLayout>} />
               <Route exact path={`/:region/committees`} element={<RegionLayout><Committees /></RegionLayout>} /> */}
-            <Route exact path={`/:region/contact`} element={<RegionLayout><Contact /></RegionLayout>} />
-            <Route exact path={`/:region/events`} element={<RegionLayout><Events /></RegionLayout>} />
-            <Route exact path={`/:region/future-events`} element={<RegionLayout><FutureEvents /></RegionLayout>} />
-            <Route exact path={`/:region/past-events`} element={<RegionLayout><PastEvents /></RegionLayout>} />
-            <Route exact path={`/:region/event-details/:eventId`} element={<RegionLayout><EventDetails /></RegionLayout>} />
-            <Route exact path={"/:region/other-event-details/:eventId"} element={<RegionLayout><NonSocietyEvent /></RegionLayout>}>
-
-            </Route>
+            <Route
+              exact
+              path={`/:region/contact`}
+              element={
+                <RegionLayout>
+                  <Contact />
+                </RegionLayout>
+              }
+            />
+            <Route
+              exact
+              path={`/:region/events`}
+              element={
+                <RegionLayout>
+                  <Events />
+                </RegionLayout>
+              }
+            />
+            <Route
+              exact
+              path={`/:region/future-events`}
+              element={
+                <RegionLayout>
+                  <FutureEvents />
+                </RegionLayout>
+              }
+            />
+            <Route
+              exact
+              path={`/:region/past-events`}
+              element={
+                <RegionLayout>
+                  <PastEvents />
+                </RegionLayout>
+              }
+            />
+            <Route
+              exact
+              path={`/:region/event-details/:eventId`}
+              element={
+                <RegionLayout>
+                  <EventDetails />
+                </RegionLayout>
+              }
+            />
+            <Route
+              exact
+              path={"/:region/other-event-details/:eventId"}
+              element={
+                <RegionLayout>
+                  <NonSocietyEvent />
+                </RegionLayout>
+              }
+            ></Route>
             <Route
               path={`/:region/event-reflection/:eventId`}
-              element={<RegionLayout><EventReflection /></RegionLayout>}
+              element={
+                <RegionLayout>
+                  <EventReflection />
+                </RegionLayout>
+              }
             />
-
 
             {/* Redirect pages */}
 
-            {!isProd() && <Route exact path={`/test`} element={<TicketComponent />} />}
+            {!isProd() && (
+              <Route exact path={`/test`} element={<TicketComponent />} />
+            )}
 
             <Route exact path={`/success`} element={<Success />} />
-            <Route exact path={`/donation/success`} element={<SuccessDonation />} />
+            <Route
+              exact
+              path={`/donation/success`}
+              element={<SuccessDonation />}
+            />
             <Route exact path={`/fail`} element={<Fail />} />
 
             {/* TICKET PURCHASE */}
-            <Route exact path={"/:region/purchase-ticket/:eventId"}
+            <Route
+              exact
+              path={"/:region/purchase-ticket/:eventId"}
               element={
                 <RegionLayout>
-                  {user && !!user.token ? <MemberPurchase /> : <GuestPurchase />}
+                  {user && !!user.token ? (
+                    <MemberPurchase />
+                  ) : (
+                    <GuestPurchase />
+                  )}
                 </RegionLayout>
               }
             />
 
             {/* Auth pages */}
             <Fragment>
-              <Route exact path={`/user`}
+              <Route
+                exact
+                path={`/user`}
                 element={
                   <AuthLayout>
                     <User />
                   </AuthLayout>
                 }
               />
-              <Route exact path={`/user/add-event`}
+              <Route
+                exact
+                path={`/user/add-event`}
                 element={
                   <AuthLayout access={ACCESS_3}>
                     <AddEvent />
                   </AuthLayout>
                 }
               />
-              <Route exact path={`/user/edit-event/:eventId`}
+              <Route
+                exact
+                path={`/user/edit-event/:eventId`}
                 element={
                   <AuthLayout access={ACCESS_3}>
                     <EditEvent />
                   </AuthLayout>
                 }
               />
-              <Route exact path={`/user/dashboard`}
+              <Route
+                exact
+                path={`/user/dashboard`}
                 element={
                   <AuthLayout access={ACCESS_3}>
                     <EventDashboard />
                   </AuthLayout>
                 }
               />
-              <Route exact path={`/user/check-guest-list`}
+              <Route
+                exact
+                path={`/user/check-guest-list`}
                 element={
                   <AuthLayout access={ACCESS_4}>
                     <GuestCheck />
@@ -321,19 +418,13 @@ const Root = () => {
             </Fragment>
 
             {/* Un-auth pages */}
-            {!user?.token && <Fragment>
-              <Route exact path={`/login`}
-                element={
-                  <LogIn />
-                }
-              />
-              <Route exact path={`/:region?/signup`}
-                element={
-                  <SignUp />
-                }
-              />
-              {/* NOTE: purchase ticket is moved to a dynamic check in the auth routes */}
-            </Fragment>}
+            {!user?.token && (
+              <Fragment>
+                <Route exact path={`/login`} element={<LogIn />} />
+                <Route exact path={`/:region?/signup`} element={<SignUp />} />
+                {/* NOTE: purchase ticket is moved to a dynamic check in the auth routes */}
+              </Fragment>
+            )}
 
             <Route exact path="/:region?" element={<Home />} />
             <Route path="*" element={<Error404 />} />
@@ -342,7 +433,7 @@ const Root = () => {
       </GlobalError>
     </BrowserRouter>
   );
-}
+};
 
 removeLogsOnProd();
 
@@ -356,5 +447,3 @@ root.render(
     </PrimeReactProvider>
   </Provider>
 );
-
-
