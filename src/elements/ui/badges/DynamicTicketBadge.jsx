954ad9be1b85f931765ca20f85f3de382dc2874e
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-10-23 07:45:37 +0300

    fix late bird

--- src/elements/ui/badges/DynamicTicketBadge.jsx
+++ src/elements/ui/badges/DynamicTicketBadge.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import SolidBadge from "./SolidBadge";
 
 const DynamicTicketBadge = ({ product }) => {
-  if (product?.earlyBird) {
+  if (product?.earlyBird === true) {
     return <SolidBadge color="#add8e6" text="Early Bird" />;
-  } else if (product?.earlyBird) {
-    <SolidBadge color="#ab1c02" text="Late Bird" />;
+  } else if (product?.lateBird === true) {
+    return <SolidBadge color="#ab1c02" text="Late Bird" />;
   } else {
     return null;
   }
