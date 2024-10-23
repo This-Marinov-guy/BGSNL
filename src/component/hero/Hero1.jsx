1eb0a3391d1c7c6f3b5c0b4f7b198aafde90cb77
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-10-23 21:43:33 +0300

    move hero

---
+++ src/component/hero/Hero1.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../redux/user";
+import { Link, useParams } from "react-router-dom";
+import ImageFb from "../../elements/ui/media/ImageFb";
+
+const Hero1 = () => {
+  const user = useSelector(selectUser);
+  const { region } = useParams();
+
+  const SlideList = [
+    {
+      textPosition: "text-center",
+      category: "",
+      title: `Bulgarian Society ${region || "Netherlands"}`,
+      description: "",
+      buttonText: user.token ? "Go To Profile" : "Become a Member",
+      style: " rn-btn-reverse-green",
+      buttonLink: user.token ? `/user` : "/signup",
+    },
+  ];
+
+  return (
+    <div
+      style={{ height: "100vh" }}
+      className="slider-activation slider-creative-agency"
+    >
+      <ImageFb
+        src={`/assets/images/bg/paralax/${region || "netherlands"}.webp`}
+        fallback={`/assets/images/bg/paralax/${region || "netherlands"}.jpg`}
+        className="home_bg"
+      />
+      {SlideList.map((value, index) => (
+        <div
+          className="slide slide-style-2 slider-paralax d-flex align-items-center justify-content-center"
+          key={index}
+        >
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className={`inner ${value.textPosition}`}>
+                  {value.category ? <span>{value.category}</span> : ""}
+                  {value.title ? (
+                    <h1 className="title theme-gradient">{value.title}</h1>
+                  ) : (
+                    ""
+                  )}
+                  {value.description ? (
+                    <p className="description">{value.description}</p>
+                  ) : (
+                    ""
+                  )}
+                  {value.buttonText && (
+                    <div className="slide-btn">
+                      <Link
+                        style={{ fontSize: "24px" }}
+                        className={"rn-button-style--2" + value.style}
+                        to={value.buttonLink}
+                      >
+                        {value.buttonText}
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Hero1;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import React, { Fragment, useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
-import { selectUser } from "../redux/user";
 import Header from "../component/header/Header";
 import AboutUs from "../component/HomeLayout/homeOne/AboutUs";
 import FooterTwo from "../component/footer/FooterTwo";
@@ -12,13 +10,12 @@ import {
   FutureEventsContent,
   FutureOtherEventsContent,
 } from "./information/FutureEvents";
-import ImageFb from "../elements/ui/media/ImageFb";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { REGIONS } from "../util/defines/REGIONS_DESIGN";
 import NewsList from "../elements/ui/lists/NewsList";
+import Hero1 from "../component/hero/Hero1";
 
 const Home = () => {
-  const user = useSelector(selectUser);
   const navigate = useNavigate();
   const { region } = useParams();
 
@@ -40,67 +37,13 @@ const Home = () => {
     }
   }, []);
 
-  const SlideList = [
-    {
-      textPosition: "text-center",
-      category: "",
-      title: `Bulgarian Society ${region || 'Netherlands'}`,
-      description: "",
-      buttonText: user.token ? "Go To Profile" : "Become a Member",
-      style: " rn-btn-reverse-green",
-      buttonLink: user.token ? `/user` : "/signup",
-    },
-  ];
-
   return (
     <Fragment>
       <Helmet pageTitle="Welcome" />
       <Header logo="light" />
+      
       {/* Start Slider Area   */}
-      <div style={{ height: '100vh' }} className="slider-activation slider-creative-agency">
-        <ImageFb
-          src={`/assets/images/bg/paralax/${region || 'netherlands'}.webp`}
-          fallback={`/assets/images/bg/paralax/${region || 'netherlands'}.jpg`}
-          className="home_bg"
-        />
-        {SlideList.map((value, index) => (
-          <div
-            className="slide slide-style-2 slider-paralax d-flex align-items-center justify-content-center"
-            key={index}
-          >
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-12">
-                  <div className={`inner ${value.textPosition}`}>
-                    {value.category ? <span>{value.category}</span> : ""}
-                    {value.title ? (
-                      <h1 className="title theme-gradient">{value.title}</h1>
-                    ) : (
-                      ""
-                    )}
-                    {value.description ? (
-                      <p className="description">{value.description}</p>
-                    ) : (
-                      ""
-                    )}
-                    {value.buttonText && (
-                      <div className="slide-btn">
-                        <Link
-                          style={{ fontSize: "24px" }}
-                          className={"rn-button-style--2" + value.style}
-                          to={value.buttonLink}
-                        >
-                          {value.buttonText}
-                        </Link>
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <Hero1 />
       {/* End Slider Area   */}
 
       {/* Start About Area  */}
