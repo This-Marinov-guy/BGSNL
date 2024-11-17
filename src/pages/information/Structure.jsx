c6ef7a72fed1df6463c22814d79b57b95b3e22b9
Vladislav Marinov <104770295+This-Marinov-guy@users.noreply.github.com>
2024-11-17 13:30:10 -0800

    Update Structure.jsx

--- src/pages/information/Structure.jsx
+++ src/pages/information/Structure.jsx
@@ -98,7 +98,7 @@ const Structure = () => {
               <div key={index} className="col-lg-6 col-md-6 col-12 m--a">
                 <div className="mb--30 mb_sm--0 center_section">
                   <ImageFb
-                    style={{ height: "15em" }}
+                    style={{ width: "200px" }}
                     className={
                       index % 2 !== 0
                         ? "team_member_border-3 mb--20"
