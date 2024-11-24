f5b48c2130c0ddadcc39024b673ef9510db4afc1
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-11-24 15:39:56 +0200

    change price ids ofr subscriptions

--- src/util/defines/REGIONS_AUTH_CONFIG.js
+++ src/util/defines/REGIONS_AUTH_CONFIG.js
@@ -2,27 +2,28 @@ import React from "react";
 import { FiUser, FiUserPlus } from "react-icons/fi";
 
 export const REGIONS_MEMBERSHIP_SPECIFICS = [
-    {
-        id: 1,
-        title: '6-months Member',
-        icon: <FiUser />,
-        description: '',
-        price: 6,
-        itemId: 'price_1OuqmtIOw5UGbAo1V4TqMet4',
-        renewItemId: 'price_1OuqmtIOw5UGbAo1V4TqMet4',
-        period: 6
-    },
-    {
-        id: 2,
-        title: 'Annual Member',
-        icon: <FiUserPlus />,
-        description: 'Be part of the society. Pay yearly for the journey with benefits such as:',
-        price: 10,
-        itemId: 'price_1Otbd6IOw5UGbAo1rdJ7wXp3',
-        renewItemId: 'price_1Otbd6IOw5UGbAo1rdJ7wXp3',
-        period: 12
-    }
-]
+  {
+    id: 1,
+    title: "6-months Member",
+    icon: <FiUser />,
+    description: "",
+    price: 6,
+    itemId: "price_1QOg1FAShinXgMFZ1dZiQn1P",
+    renewItemId: "price_1QOg1FAShinXgMFZ1dZiQn1P",
+    period: 6,
+  },
+  {
+    id: 2,
+    title: "Annual Member",
+    icon: <FiUserPlus />,
+    description:
+      "Be part of the society. Pay yearly for the journey with benefits such as:",
+    price: 10,
+    itemId: "price_1QOg1XAShinXgMFZyH0F4P9i",
+    renewItemId: "price_1QOg1XAShinXgMFZyH0F4P9i",
+    period: 12,
+  },
+];
 
 export function findMembershipByProperty(property, value) {
     return REGIONS_MEMBERSHIP_SPECIFICS.find(item => item[property] === value);
