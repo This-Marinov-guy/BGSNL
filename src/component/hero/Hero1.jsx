1449d21cf961823199e71c8f009add092c56854e
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2024-12-04 07:39:31 +0200

    add winter

--- src/component/hero/Hero1.jsx
+++ src/component/hero/Hero1.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../../redux/user";
 import { Link, useParams } from "react-router-dom";
 import ImageFb from "../../elements/ui/media/ImageFb";
+import SnowBackground from "../../elements/ui/backgrounds/SnowBackground";
 
 const Hero1 = () => {
   const user = useSelector(selectUser);
@@ -25,6 +26,7 @@ const Hero1 = () => {
       style={{ height: "100vh" }}
       className="slider-activation slider-creative-agency"
     >
+      <SnowBackground />
       <ImageFb
         src={`/assets/images/bg/paralax/${region || "netherlands"}.webp`}
         fallback={`/assets/images/bg/paralax/${region || "netherlands"}.jpg`}
---
+++ src/elements/ui/backgrounds/SnowBackground.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Snowfall from "react-snowfall";
+import { HOLIDAYS } from "../../../util/configs/common";
+
+const SnowBackground = () => {
+  if (!HOLIDAYS.isWinter) {
+    return;
+  }
+  
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        position: "absolute",
+        zIndex: "10000",
+      }}
+    >
+      <Snowfall snowflakeCount={200} />
+    </div>
+  );
+};
+
+export default SnowBackground;
