b73dec569f4d24028f5d0fcf7b32d14bafcac220
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-09-20 23:52:47 +0300

    final updates

--- src/elements/ui/cards/UserCard.jsx
+++ src/elements/ui/cards/UserCard.jsx
@@ -18,86 +18,95 @@ const UserCard = ({ user }) => {
   return (
     <div className="service gradient-border-2 mt--20">
       {/* <h2 className='archive-title' >Greetings, {user.name}!</h2> */}
-      <div className="hor_section mb--10">
+      {/* <div className="hor_section mb--10">
         <p className="mt--10" style={{ fontFamily: "Archive" }}>
-          {capitalizeFirstLetter(user.region)} <br /> {formatRole(user.roles)}{" "}
+          {capitalizeFirstLetter(user.region, true)} <br />{" "}
+          {formatRole(user.roles)}{" "}
         </p>
-      </div>
+      </div> */}
       <div className="pricing-body">
-        <ul style={{ textAlign: "start" }} className="list-style--1">
-          <li>
-            <span className="bold">Full Name: </span>
-            {user.name + " " + user.surname}
-          </li>
-          {user?.birth && (
-            <li>
-              <span className="bold">Date of Birth: </span>
-              {moment(user.birth).format("D MMM YYYY")}
-            </li>
-          )}
-          <li>
-            <span className="bold">Email: </span>
-            {user.email}
-          </li>
-          {user?.phone && (
-            <li>
-              <span className="bold">Phone: </span>
-              {user.phone}
-            </li>
-          )}
-          {user?.university && (
-            <li>
-              <span className="bold">University: </span>
-              {user.university === "other"
-                ? user.otherUniversityName
-                : user.university}
-            </li>
-          )}
+        <div className="user-info-grid">
+          {/* Personal Information Column */}
+          <div className="user-info-column">
+            <h4 className="column-title">Personal Information</h4>
+            <ul className="list-style--1">
+              <li>
+                <span className="bold">Full Name: </span>
+                {user.name + " " + user.surname}
+              </li>
+              {user?.birth && (
+                <li>
+                  <span className="bold">Date of Birth: </span>
+                  {moment(user.birth).format("D MMM YYYY")}
+                </li>
+              )}
+              <li>
+                <span className="bold">Email: </span>
+                {user.email}
+              </li>
+              {user?.phone && (
+                <li>
+                  <span className="bold">Phone: </span>
+                  {user.phone}
+                </li>
+              )}
+              {user?.university && (
+                <li>
+                  <span className="bold">University: </span>
+                  {user.university === "other"
+                    ? user.otherUniversityName
+                    : user.university}
+                </li>
+              )}
+            </ul>
+          </div>
 
-          {/* <li style={{ fontWeight: "bold" }}>
-                        <FiCircle style={{ fontSize: "14px" }} /> Membership
-                        Expires: {user.expireDate}
-                      </li> */}
-          {REGION_WHATSAPP[user.region] && (
-            <li>
-              <FiCheckCircle style={{ fontSize: "14px" }} />
-              <a
-                style={{ color: "#017363" }}
-                href={REGION_WHATSAPP[user.region]}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Click here to join the Regional Member Chat
-              </a>
-            </li>
-          )}
-          {isAlumni ? (
-            <li>
-              <FiCheckCircle style={{ fontSize: "14px" }} />
-              <a
-                style={{ color: "#017363" }}
-                href={REGION_WHATSAPP["alumni"]}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Click here to join the Alumni Chat
-              </a>
-            </li>
-          ) : (
-            <li>
-              <FiCheckCircle style={{ fontSize: "14px" }} />
-              <a
-                style={{ color: "#017363" }}
-                href={REGION_WHATSAPP["netherlands"]}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Click here to join the National Member Chat
-              </a>
-            </li>
-          )}
-        </ul>
-        
+          {/* Community Links Column */}
+          <div className="user-info-column">
+            <h4 className="column-title">Community</h4>
+            <ul className="list-style--1">
+              {REGION_WHATSAPP[user.region] && (
+                <li className="d-flex align-items-center">
+                  <FiCheckCircle className="link-icon" />
+                  <a
+                    className="community-link"
+                    href={REGION_WHATSAPP[user.region]}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Regional Member Chat
+                  </a>
+                </li>
+              )}
+              {isAlumni ? (
+                <li className="d-flex align-items-center">
+                  <FiCheckCircle className="link-icon" />
+                  <a
+                    className="community-link"
+                    href={REGION_WHATSAPP["alumni"]}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Alumni Chat
+                  </a>
+                </li>
+              ) : (
+                <li>
+                  <FiCheckCircle className="link-icon" />
+                  <a
+                    className="community-link"
+                    href={REGION_WHATSAPP["netherlands"]}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    National Member Chat
+                  </a>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+
         {/* Become an Alumni Button */}
         {!isAlumni && (
           <div style={{ marginTop: "20px", textAlign: "center" }}>
@@ -110,14 +119,13 @@ const UserCard = ({ user }) => {
             </button>
           </div>
         )}
-        
       </div>
 
       {/* Alumni Program Modal */}
       <AlumniModal
         isOpen={isAlumniModalOpen}
         onClose={() => setIsAlumniModalOpen(false)}
-        onJoinNow={() => window.location.href = "/alumni/register"}
+        onJoinNow={() => (window.location.href = "/alumni/register")}
       />
     </div>
   );
