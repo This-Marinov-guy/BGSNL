ef2e57789270bc2e45836c3384906dcbc8d9e87e
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2025-10-28 08:16:14 +0200

    update contact form

--- src/elements/contact/ContactForm.js
+++ src/elements/contact/ContactForm.js
@@ -1,29 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import emailjs from "emailjs-com";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import Loader from "../ui/loading/Loader";
+import { REGION_EMAIL } from "../../util/defines/REGIONS_DESIGN";
 
-const Result = () => {
-  return (
-    <p className="success-message">
-      Your Message has been successfully sent. I will contact you soon.
-    </p>
-  );
-};
-
-const FailResult = () => {
-  return (
-    <p className="fail-message">
-      Your Message could not be sent. Please try again.
-    </p>
-  );
-};
 function ContactForm(props) {
   const [result, showresult] = useState(null);
   const { region = "netherlands" } = useParams();
   const [isLoading, setIsLoading] = useState(false);
   const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const Result = () => {
+    return (
+      <p className="success-message">
+        Your Message has been successfully sent. We will come back to your message soon!
+      </p>
+    );
+  };
+
+  const FailResult = () => {
+    return (
+      <p className="fail-message">
+        Your Message could not be sent. Please try again or email us at{" "}
+        <a
+          href={`mailto:${REGION_EMAIL[region ?? "netherlands"]}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {REGION_EMAIL[region ?? "netherlands"]}
+        </a>
+      </p>
+    );
+  };
 
   const sendEmail = async (e) => {
     setIsLoading(true);
@@ -84,7 +93,7 @@ function ContactForm(props) {
       </div>
 
       {/* Hidden field for region */}
-      <input type="hidden" name="region" value={region ?? 'netherlands'} />
+      <input type="hidden" name="region" value={region ?? "netherlands"} />
 
       {!isLoading && !isSubmitted && (
         <div className="rn-form-group">
