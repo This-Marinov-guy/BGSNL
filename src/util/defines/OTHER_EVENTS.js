318bb0e02782ec371d6008af68b7d3d4b651f8ba
Vladislav Marinov <104770295+This-Marinov-guy@users.noreply.github.com>
2026-01-08 16:00:20 +0200

    remove pwc event

--- src/util/defines/OTHER_EVENTS.js
+++ src/util/defines/OTHER_EVENTS.js
@@ -2,87 +2,87 @@ import React from "react";
 import { Field, ErrorMessage } from "formik";
 
 export const OTHER_EVENTS = [
-  {
-    region: "",
-    membersOnly: false,
-    visible: true,
-    title: "PwC Offer in a day",
-    // newTitle: "Career Day",
-    // description: "Deloitte Bulgaria",
-    bgImage: "4",
-    bgImageExtra: "/assets/images/events/portfolio-2.3.png",
-    date: "8th January",
-    time: "TBD",
-    // 1 hour earlier than Netherlands / 2 than Bulgaria
-    timeStamp: "2025-01-07T13:00:00",
-    ticketTimer: "2025-01-07T13:00:00",
-    ticketLimit: 250,
-    //   Use the corrected date and time for changes in the date or time. Do not change the initial ones as it will make a new event in the DB
-    correctedDate: "",
-    correctedTime: "",
-    where: `PwC office (9-11 Maria Louisa Blvd.) Sofia`,
-    isMemberFree: true,
-    entry: "Free",
-    memberEntry: "Free",
-    ticketLink:
-      "https://jobs-cee.pwc.com/ce/en/job/687549WD/Offer-in-a-day-Financial-and-Audit-Analyst",
-    // including: ["(+ drink and snack)", "(+ drink and snack)"],
-    // ticketLink:
-    //   "https://www.tickettailor.com/events/hellenicassociationgroningen/1156915",
-    // price_id: "price_1P7uhsIOw5UGbAo1fvnUj3yw",
-    // memberPrice_id: "price_1P7uhsIOw5UGbAo1fvnUj3yw",
-    // activeMemberPrice_id: "price_1P7uhsIOw5UGbAo1fvnUj3yw",
-    text: `
-<p>
-    We would like to invite you to the <strong>second edition of our one-of-a-kind recruitment event</strong> at the PwC Bulgaria office – 
-    <strong>“Offer in a Day”</strong>.
-  </p>
+//   {
+//     region: "",
+//     membersOnly: false,
+//     visible: true,
+//     title: "PwC Offer in a day",
+//     // newTitle: "Career Day",
+//     // description: "Deloitte Bulgaria",
+//     bgImage: "4",
+//     bgImageExtra: "/assets/images/events/portfolio-2.3.png",
+//     date: "8th January",
+//     time: "TBD",
+//     // 1 hour earlier than Netherlands / 2 than Bulgaria
+//     timeStamp: "2025-01-07T13:00:00",
+//     ticketTimer: "2025-01-07T13:00:00",
+//     ticketLimit: 250,
+//     //   Use the corrected date and time for changes in the date or time. Do not change the initial ones as it will make a new event in the DB
+//     correctedDate: "",
+//     correctedTime: "",
+//     where: `PwC office (9-11 Maria Louisa Blvd.) Sofia`,
+//     isMemberFree: true,
+//     entry: "Free",
+//     memberEntry: "Free",
+//     ticketLink:
+//       "https://jobs-cee.pwc.com/ce/en/job/687549WD/Offer-in-a-day-Financial-and-Audit-Analyst",
+//     // including: ["(+ drink and snack)", "(+ drink and snack)"],
+//     // ticketLink:
+//     //   "https://www.tickettailor.com/events/hellenicassociationgroningen/1156915",
+//     // price_id: "price_1P7uhsIOw5UGbAo1fvnUj3yw",
+//     // memberPrice_id: "price_1P7uhsIOw5UGbAo1fvnUj3yw",
+//     // activeMemberPrice_id: "price_1P7uhsIOw5UGbAo1fvnUj3yw",
+//     text: `
+// <p>
+//     We would like to invite you to the <strong>second edition of our one-of-a-kind recruitment event</strong> at the PwC Bulgaria office – 
+//     <strong>“Offer in a Day”</strong>.
+//   </p>
 
-  <p>
-    Say goodbye to the tedious recruitment process and seize the opportunity to receive an 
-    <strong>job offer from PwC Bulgaria within just 24 hours</strong>.
-  </p>
+//   <p>
+//     Say goodbye to the tedious recruitment process and seize the opportunity to receive an 
+//     <strong>job offer from PwC Bulgaria within just 24 hours</strong>.
+//   </p>
 
-  <p><strong>If you’re ready to launch your career in Audit and Finance, join us:</strong></p>
+//   <p><strong>If you’re ready to launch your career in Audit and Finance, join us:</strong></p>
 
-  <ul>
-    <li>📅 <strong>Event Date:</strong> 8th January</li>
-    <li>📍 <strong>Location:</strong> PwC office (9–11 Maria Louisa Blvd.)</li>
-  </ul>
+//   <ul>
+//     <li>📅 <strong>Event Date:</strong> 8th January</li>
+//     <li>📍 <strong>Location:</strong> PwC office (9–11 Maria Louisa Blvd.)</li>
+//   </ul>
 
-  <h3>🎯 Who are we looking for?</h3>
-  <ul>
-    <li>Final-year students and recent graduates in <strong>Finance, Accounting, Business Administration, Economics, Management</strong>, or related fields</li>
-    <li>Available to join our team between <strong>February and June 2026</strong></li>
-    <li>Fluent in <strong>Bulgarian and English</strong> (a second foreign language is a plus)</li>
-  </ul>
+//   <h3>🎯 Who are we looking for?</h3>
+//   <ul>
+//     <li>Final-year students and recent graduates in <strong>Finance, Accounting, Business Administration, Economics, Management</strong>, or related fields</li>
+//     <li>Available to join our team between <strong>February and June 2026</strong></li>
+//     <li>Fluent in <strong>Bulgarian and English</strong> (a second foreign language is a plus)</li>
+//   </ul>
 
-  <h3>🗓️ What’s on the agenda?</h3>
-  <ul>
-    <li>👋 Welcome &amp; meet the team</li>
-    <li>💼 Group work – experience a day in the life of PwC Bulgaria</li>
-    <li>🗣️ Connect with peers and mentors</li>
-    <li>💡 Individual catch-ups – 1:1 sessions with personalized mentor feedback</li>
-  </ul>
+//   <h3>🗓️ What’s on the agenda?</h3>
+//   <ul>
+//     <li>👋 Welcome &amp; meet the team</li>
+//     <li>💼 Group work – experience a day in the life of PwC Bulgaria</li>
+//     <li>🗣️ Connect with peers and mentors</li>
+//     <li>💡 Individual catch-ups – 1:1 sessions with personalized mentor feedback</li>
+//   </ul>
 
-  <h3>✨ Why should you attend?</h3>
-  <ul>
-    <li>🔸 Opportunity to receive a <strong>job offer within 24 hours</strong></li>
-    <li>🔸 Meet our team and experience the <strong>dynamic PwC work culture</strong></li>
-    <li>🔸 Explore exciting career paths in <strong>Audit, Finance, and Sustainability</strong></li>
-  </ul>
+//   <h3>✨ Why should you attend?</h3>
+//   <ul>
+//     <li>🔸 Opportunity to receive a <strong>job offer within 24 hours</strong></li>
+//     <li>🔸 Meet our team and experience the <strong>dynamic PwC work culture</strong></li>
+//     <li>🔸 Explore exciting career paths in <strong>Audit, Finance, and Sustainability</strong></li>
+//   </ul>
 
-  <p>
-    📝 <strong>Application Deadline:</strong> 6th January 2026
-  </p>
-    `,
-    freePass: ["vladislavmarinov3142@gmail.com"],
-    marketingInputs: false,
-    ticket_img: "/assets/images/events/ticket-2.png",
-    images: [
-      // "/assets/images/events/portfolio-2.jpeg",
-      "/assets/images/events/portfolio-2.4.png",
-    ],
-    poster: "/assets/images/events/portfolio-2.4.png",
-  },
+//   <p>
+//     📝 <strong>Application Deadline:</strong> 6th January 2026
+//   </p>
+//     `,
+//     freePass: ["vladislavmarinov3142@gmail.com"],
+//     marketingInputs: false,
+//     ticket_img: "/assets/images/events/ticket-2.png",
+//     images: [
+//       // "/assets/images/events/portfolio-2.jpeg",
+//       "/assets/images/events/portfolio-2.4.png",
+//     ],
+//     poster: "/assets/images/events/portfolio-2.4.png",
+//   },
 ];
