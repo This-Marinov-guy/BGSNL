28067633cce12621daf21fb2219343aa579795f7
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2026-04-06 19:07:28 +0300

    add promo codes

--- src/util/defines/PROMO_CODES.js
+++ src/util/defines/PROMO_CODES.js
@@ -1,8 +1,14 @@
 export const PROMO_CODES = {
   groningen: {
-    name: "Deizi",
+    name: "Deizi - BG store",
     code: "STUDENTNL",
     link: "https://deizi-the-bulgarian-store.sumupstore.com",
-    discount: '15%',
+    discount: "15%",
+  },
+  netherlands: {
+    name: "Signaal - accommodation app",
+    code: "ADOMKN",
+    link: "https://signaal.app/download",
+    discount: "10%",
   },
 };
