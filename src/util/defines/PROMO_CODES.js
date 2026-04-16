aba3f628d31742ad6aa7af1b1ed8cacee857c61f
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2026-04-16 21:29:56 +0300

    update promom codes

--- src/util/defines/PROMO_CODES.js
+++ src/util/defines/PROMO_CODES.js
@@ -1,14 +1,24 @@
 export const PROMO_CODES = {
-  groningen: {
-    name: "Deizi - BG store",
-    code: "STUDENTNL",
-    link: "https://deizi-the-bulgarian-store.sumupstore.com",
-    discount: "15%",
-  },
-  netherlands: {
-    name: "Signaal - accommodation app",
-    code: "ADOMKN",
-    link: "https://signaal.app/download",
-    discount: "10%",
-  },
+  groningen: [
+    {
+      name: "Deizi - BG store",
+      code: "STUDENTNL",
+      link: "https://deizi-the-bulgarian-store.sumupstore.com",
+      discount: "15%",
+    },
+    {
+      name: "Uno",
+      code: "BGSG25",
+      link: "https://uno-groningen.nl",
+      discount: "25%",
+    },
+  ],
+  netherlands: [
+    {
+      name: "Signaal - accommodation app",
+      code: "ADOMKN",
+      link: "https://signaal.app/download",
+      discount: "10%",
+    },
+  ],
 };
