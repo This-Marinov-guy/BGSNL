07e9a96dae132918b372da5754a4a83dcc28599e
This-Marinov-guy <vladislavmarinov3142@gmail.com>
2026-04-21 14:22:23 +0300

    update

--- src/elements/legals/Terms.jsx
+++ src/elements/legals/Terms.jsx
@@ -8,94 +8,172 @@ const Terms = () => {
       <div className="mt--80 mb--80">
         <h3>1. Introduction</h3>
         <p className="ml--20">
-          These Terms and Conditions govern your use of the Bulgarian Society Netherlands website (https://www.bulgariansociety.nl). By accessing or using this website, you agree to comply with these terms. If you do not agree, please refrain from using the site.
+          These Terms and Conditions govern your use of the Bulgarian Society
+          Netherlands website (https://www.bulgariansociety.nl). By accessing or
+          using this website, you agree to comply with these terms. If you do
+          not agree, please refrain from using the site.
         </p>
-        
+
         <h3>2. Membership and Access</h3>
         <p className="ml--20">
-          Membership of the Bulgarian Society Netherlands is subject to the eligibility criteria and procedures outlined in the association&apos;s statutes. Membership status includes Alumni members and regular members of the association. Membership status shall be conferred only upon receipt of the full membership fee. Certain features of this website are accessible only to members. By attempting to access member-only sections, you confirm that you are an active member in good standing. Unauthorized access is strictly prohibited.
+          Membership of the Bulgarian Society Netherlands is subject to the
+          eligibility criteria and procedures outlined in the association&apos;s
+          statutes. Membership status includes Alumni members and regular
+          members of the association. Membership status shall be conferred only
+          upon receipt of the full membership fee. Certain features of this
+          website are accessible only to members. By attempting to access
+          member-only sections, you confirm that you are an active member in
+          good standing. Unauthorized access is strictly prohibited.
         </p>
-        
+
         <p className="ml--20">
-          <strong>Eligibility:</strong> Membership is available to individuals who meet the statutory requirements of the Bulgarian Society Netherlands, including residency conditions and tertiary status.
+          <strong>Eligibility:</strong> Membership is available to individuals
+          who meet the statutory requirements of the Bulgarian Society
+          Netherlands, including residency conditions and tertiary status.
         </p>
-        
+
         <p className="ml--20">
-          <strong>Validity Period:</strong> Membership status and their benefits are valid only during the active paid membership term. Alumni membership status continues indefinitely until ended by the account owner.
+          <strong>Validity Period:</strong> Membership status and their benefits
+          are valid only during the active paid membership term. Alumni
+          membership status continues indefinitely until ended by the account
+          owner.
         </p>
-        
+
         <p className="ml--20">
-          <strong>Non-Transferability:</strong> Membership is personal, non-transferable, and may not be shared, reproduced, or used for commercial purposes.
+          <strong>Non-Transferability:</strong> Membership is personal,
+          non-transferable, and may not be shared, reproduced, or used for
+          commercial purposes.
         </p>
-        
+
         <p className="ml--20">
-          <strong>Prohibited Activities:</strong> Actions such as resale, duplication, or misuse of membership benefits are strictly forbidden.
+          <strong>Prohibited Activities:</strong> Actions such as resale,
+          duplication, or misuse of membership benefits are strictly forbidden.
         </p>
 
         <h3>2.2 Responsibilities of Members</h3>
         <p className="ml--20">
-          Upon receiving a membership status, members are responsible to provide accurate personal information. Members must respect the rules of the association when using membership benefits. Members may enjoy discounts, exclusive access to events, and other benefits as determined by Bulgarian Society Netherlands.
+          Upon receiving a membership status, members are responsible to provide
+          accurate personal information. Members must respect the rules of the
+          association when using membership benefits. Members may enjoy
+          discounts, exclusive access to events, and other benefits as
+          determined by Bulgarian Society Netherlands.
         </p>
 
         <h3>2.4 Fees and Payment</h3>
         <p className="ml--20">
-          Regular membership fees are billed either annually or semi-annually, depending on the membership type selected by the member at the time of enrollment. These payments are not automatically deducted. Members are responsible for manually submitting payment by the due date in order to maintain active membership status. Alumni membership fees are automatically renewed and deducted from the account on file at each renewal period. This automatic renewal continues indefinitely unless the account holder cancels the membership. The association reserves the right to modify membership fees after a notice to all members.
+          Membership fees are billed monthly, annually or semi-annually,
+          depending on the membership type selected by the client at the time of
+          enrollment. These payments are automatically deducted based on the
+          start date and period the client has selected. Members are responsible
+          for manually updating their payment methods in order to maintain
+          active membership status. All membership fees are automatically
+          renewed and deducted from the account on file at each renewal period.
+          This automatic renewal continues indefinitely unless the account
+          holder cancels the membership. The association reserves the right to
+          modify membership fees after a notice to all clients.
         </p>
 
         <h3>2.5 Cancellation, Suspension, and Termination</h3>
         <p className="ml--20">
-          Both Regular and Alumni memberships are renewed automatically upon successful deduction of the membership fee from the member&apos;s designated bank account. It may be cancelled at any time by the account holder. Alumni membership continues until explicitly terminated by the account owner. Upon termination, all membership benefits and access rights are revoked, and fees already paid are non-refundable.
+          Both Regular and Alumni memberships are renewed automatically upon
+          successful deduction of the membership fee from the member&apos;s
+          designated bank account. It may be cancelled at any time by the
+          account holder. Alumni membership continues until explicitly
+          terminated by the account owner. Upon termination, all membership
+          benefits and access rights are revoked, and fees already paid are
+          non-refundable.
         </p>
-        
+
         <p className="ml--20">
-          The association reserves the right to suspend or terminate membership for non-payment, misuse of membership privileges, or conduct detrimental to the community.
+          The association reserves the right to suspend or terminate membership
+          for non-payment, misuse of membership privileges, or conduct
+          detrimental to the community.
         </p>
 
         <h3>3. Use of Website</h3>
         <p className="ml--20">
-          You agree to use this website only for lawful purposes, in ways that do not infringe upon the rights of others or interfere with their use and enjoyment of the site.
+          You agree to use this website only for lawful purposes, in ways that
+          do not infringe upon the rights of others or interfere with their use
+          and enjoyment of the site.
         </p>
 
         <h3>4. Intellectual Property</h3>
         <p className="ml--20">
-          All content on this website, including text, images, logos, and graphics, is the property of the Bulgarian Society Netherlands and is protected by copyright laws. Unauthorized use of any content is prohibited.
+          All content on this website, including text, images, logos, and
+          graphics, is the property of the Bulgarian Society Netherlands and is
+          protected by copyright laws. Unauthorized use of any content is
+          prohibited.
         </p>
 
         <h3>5. Limitation of Liability</h3>
         <p className="ml--20">
-          The Bulgarian Society Netherlands strives to ensure that the information on this website is accurate and up to date. However, we do not guarantee the accuracy, completeness, or reliability of the content. We reserve our right to change membership pricing pursuant to changes of the statutes, as well as of pricing of events in accordance with board decisions. No liability should arise from such changes.
+          The Bulgarian Society Netherlands strives to ensure that the
+          information on this website is accurate and up to date. However, we do
+          not guarantee the accuracy, completeness, or reliability of the
+          content. We reserve our right to change membership pricing pursuant to
+          changes of the statutes, as well as of pricing of events in accordance
+          with board decisions. No liability should arise from such changes.
         </p>
 
         <h3>6. Links to Third-Party Websites</h3>
         <p className="ml--20">
-          This website may contain links to external sites. The Bulgarian Society Netherlands is not responsible for the content or privacy practices of these third-party websites.
+          This website may contain links to external sites. The Bulgarian
+          Society Netherlands is not responsible for the content or privacy
+          practices of these third-party websites.
         </p>
 
         <h3>7. Governing Law</h3>
         <p className="ml--20">
-          These Terms and Conditions are governed by Dutch law. Any disputes arising from the use of this website shall be subject to the exclusive jurisdiction of the courts in the Netherlands.
+          These Terms and Conditions are governed by Dutch law. Any disputes
+          arising from the use of this website shall be subject to the exclusive
+          jurisdiction of the courts in the Netherlands.
         </p>
 
         <h3>8. Refund Policy</h3>
         <p className="ml--20">
-          <strong>Event Ticket Refunds:</strong> Refunds for event tickets are possible only when requested at least 48 hours before the scheduled start time of the event. Refunds must be explicitly requested by the ticket holder.
+          <strong>Event Ticket Refunds:</strong> Refunds for event tickets are
+          possible only when requested at least 48 hours before the scheduled
+          start time of the event. Refunds must be explicitly requested by the
+          ticket holder.
         </p>
-        
+
         <p className="ml--20">
-          <strong>Exceptions to the 48-Hour Rule:</strong> In cases where the organizers make last-minute changes to the event (such as date, time, location, or cancellation), the 48-hour requirement does not apply, and refund requests will be considered on a case-by-case basis.
+          <strong>Exceptions to the 48-Hour Rule:</strong> In cases where the
+          organizers make last-minute changes to the event (such as date, time,
+          location, or cancellation), the 48-hour requirement does not apply,
+          and refund requests will be considered on a case-by-case basis.
         </p>
-        
+
         <p className="ml--20">
-          <strong>No Automatic Refunds:</strong> Refunds are only executed upon explicit request. Failure to request a refund within the applicable timeframe will result in forfeiture of the ticket fee.
+          <strong>No Automatic Refunds:</strong> Refunds are only executed upon
+          explicit request. Failure to request a refund within the applicable
+          timeframe will result in forfeiture of the ticket fee.
         </p>
-        
+
         <p className="ml--20">
-          <strong>Processing:</strong> Approved refunds will be processed using the same payment method used for the original purchase and may take 5-10 business days to reflect in your account.
+          <strong>No Membership Refunds:</strong>
+          Clients are solely responsible for managing and canceling their
+          memberships prior to renewal. While BGSNL reserves the right to cancel
+          or suspend subscriptions in cases of terms violations or at the
+          client’s request, BGSNL assumes no responsibility for recurring
+          charges incurred due to the client’s failure to cancel before the
+          applicable billing date. All payments processed for a renewed billing
+          period are final and non-refundable, including where the renewal was
+          unintended by the client.
+        </p>
+
+        <p className="ml--20">
+          <strong>Processing:</strong> Approved refunds will be processed using
+          the same payment method used for the original purchase and may take
+          5-10 business days to reflect in your account.
         </p>
 
         <h3>9. Changes to the terms and conditions</h3>
         <p className="ml--20">
-          These Terms and Conditions are regularly updated to reflect the current legal situation. By using this website, you agree to adhere to the terms and conditions. Any updates will be reflected on the website accordingly.
+          These Terms and Conditions are regularly updated to reflect the
+          current legal situation. By using this website, you agree to adhere to
+          the terms and conditions. Any updates will be reflected on the website
+          accordingly.
         </p>
       </div>
     </div>
